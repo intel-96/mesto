b3ac06419ea2d4d0178667a66a0602377ea53e37
Kirill Raskopin <Intel-96@yandex.ru>
2020-11-25 19:31:41 +0300

    1.0

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let closeButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.popup__input_username');
+let jobInput = document.querySelector('.popup__input_subtitle');
+
+// Открытие попапа
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+  editButton.addEventListener('click', () => openPopup(popup));
+
+// Закрытие попапа
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+  closeButton.addEventListener('click', () => closePopup(popup));
+
+// Заполнение формы данными со страницы
+function fillProfileInputs() {
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+editButton.addEventListener('click', () => fillProfileInputs());
+
+// Редактирование формы изменения профиля
+let formElement = document.querySelector('.popup__form')
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closePopup();
+}
+  formElement.addEventListener('submit', formSubmitHandler);
